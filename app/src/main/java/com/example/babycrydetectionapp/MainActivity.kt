bc3246e39ed1b55870ab7155027b46fa65c04d27
guustek <guustek@wp.pl>
2022-03-19 01:06:50 +0100

    demo klasyfikujące dźwięki

--- app/src/main/java/com/example/babycrydetectionapp/MainActivity.kt
+++ app/src/main/java/com/example/babycrydetectionapp/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.example.babycrydetectionapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.AudioRecord
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.os.HandlerCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.babycrydetectionapp.databinding.ActivityMainBinding
+import org.tensorflow.lite.task.audio.classifier.AudioClassifier
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_RECORD_AUDIO = 2137
+        private const val MODEL_FILE = "yamnet.tflite"
+        private const val MINIMUM_DISPLAY_THRESHOLD = 0.3f
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private val probabilitiesAdapter by lazy { ProbabilitiesAdapter() }
+
+    private val audioClassifier: AudioClassifier by lazy { AudioClassifier.createFromFile(this, MODEL_FILE) }
+    private lateinit var audioRecord: AudioRecord
+    private lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            probabilitiesList.apply {
+                adapter = probabilitiesAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+
+        requestMicrophonePermission()
+        audioRecord = audioClassifier.createAudioRecord()
+        audioRecord.startRecording()
+
+        val handlerThread = HandlerThread("classificationThread")
+        handlerThread.start()
+        handler = HandlerCompat.createAsync(handlerThread.looper)
+
+        val classification = Runnable {
+            val audioTensor = audioClassifier.createInputTensorAudio()
+            audioTensor.load(audioRecord)
+            val output = audioClassifier.classify(audioTensor)
+            val filteredModelOutput = output[0].categories.filter {
+                it.score > MINIMUM_DISPLAY_THRESHOLD
+            }.sortedBy {
+                -it.score
+            }
+
+            runOnUiThread {
+                probabilitiesAdapter.categoryList = filteredModelOutput
+                probabilitiesAdapter.notifyDataSetChanged()
+            }
+        }
+
+        binding.classifyButton.setOnClickListener {
+            handler.post(classification)
+        }
+    }
+
+    private fun requestMicrophonePermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO), REQUEST_RECORD_AUDIO)
+        }
     }
 }
---
+++ app/src/main/java/com/example/babycrydetectionapp/ProbabilitiesAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.babycrydetectionapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.babycrydetectionapp.databinding.ProbabilitiesListItemBinding
+import org.tensorflow.lite.support.label.Category
+
+class ProbabilitiesAdapter : RecyclerView.Adapter<ProbabilitiesAdapter.ViewHolder>() {
+    var categoryList: List<Category> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ProbabilitiesListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val category = categoryList[position]
+        holder.bind(position, category.label, category.score)
+    }
+
+    override fun getItemCount() = categoryList.size
+
+    class ViewHolder(private val binding: ProbabilitiesListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int, label: String, score: Float) {
+            with(binding) {
+                probabilityClassText.text = label
+                val newValue = (score * 100).toInt()
+                probabilityValueBar.progress = newValue
+            }
+        }
+    }
+}
