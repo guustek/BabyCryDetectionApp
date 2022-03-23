ddeefafa06e02133f29e444bec37f56204c97504
guustek <guustek@wp.pl>
2022-03-23 21:25:03 +0100

    SMS wysyłany po wykryciu mowy v2

--- app/src/main/java/com/example/babycrydetectionapp/MainActivity.kt
+++ app/src/main/java/com/example/babycrydetectionapp/MainActivity.kt
@@ -6,22 +6,32 @@ import android.media.AudioRecord
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
-import android.view.View
-import android.widget.ProgressBar
+import android.os.SystemClock
+import android.telephony.PhoneNumberUtils
+import android.telephony.SmsManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 import androidx.core.os.HandlerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.babycrydetectionapp.MainActivity.Companion.REQUEST_RECORD_AUDIO
+import com.example.babycrydetectionapp.MainActivity.Companion.REQUEST_SEND_SMS
 import com.example.babycrydetectionapp.databinding.ActivityMainBinding
 import org.tensorflow.lite.task.audio.classifier.AudioClassifier
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         const val REQUEST_RECORD_AUDIO = 2137
+        const val REQUEST_SEND_SMS = 2115
         private const val MODEL_FILE = "yamnet.tflite"
         private const val MINIMUM_DISPLAY_THRESHOLD = 0.3f
     }
+
+    private var classificationInterval = 500L
 
     private lateinit var binding: ActivityMainBinding
     private val probabilitiesAdapter by lazy { ProbabilitiesAdapter() }
@@ -43,35 +53,73 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // zażądanie pozwolenia na nagrywanie audio
         requestMicrophonePermission()
+        requestSmsSendingPermission()
+        // utworzenie audio rekordera na podstawie formatu wymaganego przez klasyfikator
         audioRecord = audioClassifier.createAudioRecord()
-        audioRecord.startRecording()
 
+        // utworzenie handlera oddzielnego wątku klasyfikacji
         val handlerThread = HandlerThread("classificationThread")
         handlerThread.start()
         handler = HandlerCompat.createAsync(handlerThread.looper)
 
-        val classification = Runnable {
-            runOnUiThread { binding.progressBar.visibility = View.VISIBLE }
+        binding.classifyButton.setOnClickListener {
+            startListening()
+        }
+    }
 
-            val audioTensor = audioClassifier.createInputTensorAudio()
-            audioTensor.load(audioRecord)
-            val output = audioClassifier.classify(audioTensor)
-            val filteredModelOutput = output[0].categories.filter {
-                it.score > MINIMUM_DISPLAY_THRESHOLD
-            }.sortedBy {
-                -it.score
-            }
+    private fun startListening() {
+        binding.timer.start()
+        binding.classifyButton.text = "Stop"
+        binding.classifyButton.setOnClickListener {
+            stopListening()
+        }
+        val classification = object : Runnable {
+            override fun run() {
+                // tworzy obiekt przechowujący dane do klasyfikacji
+                val tensorAudio = audioClassifier.createInputTensorAudio()
+                // ładuje próbkę dźwięku
+                tensorAudio.load(audioRecord)
+                // klasyfikacja
+                val output = audioClassifier.classify(tensorAudio)
+                // filtrowanie (tylko kategorie z prawdopodobieństwem > 0.3)
+                val filteredOutput = output[0].categories.filter {
+                    it.score > MINIMUM_DISPLAY_THRESHOLD
+                }.sortedBy {
+                    -it.score
+                }
+                //Baby cry, infant cry
+                if (filteredOutput.any { it.label == "Speech" }) {
+                    runOnUiThread { stopListening() }
+                    val smsManager = SmsManager.getDefault() as SmsManager
 
-            runOnUiThread {
-                probabilitiesAdapter.categoryList = filteredModelOutput
-                probabilitiesAdapter.notifyDataSetChanged()
-                binding.progressBar.visibility = View.INVISIBLE
+                    val number = PhoneNumberUtils.formatNumber(binding.phoneNumber.text.toString(), Locale.getDefault().country)
+                    //5554 to numer mojego emulatora u was może być inaczej
+                    smsManager.sendTextMessage("5554", null, "TEST", null, null)
+                }
+
+                runOnUiThread {
+                    probabilitiesAdapter.categoryList = filteredOutput
+                    probabilitiesAdapter.notifyDataSetChanged()
+                }
+                // Rerun the classification after a certain interval
+                handler.postDelayed(this, classificationInterval)
             }
         }
+        // rozpoczęcie nasłuchiwania
+        audioRecord.startRecording()
+        handler.post(classification)
+    }
 
+    private fun stopListening() {
+        binding.timer.base = SystemClock.elapsedRealtime();
+        binding.timer.stop()
+        audioRecord.stop()
+        handler.removeCallbacksAndMessages(null)
+        binding.classifyButton.text = "Listen"
         binding.classifyButton.setOnClickListener {
-            handler.post(classification)
+            startListening()
         }
     }
 
@@ -83,6 +131,17 @@ class MainActivity : AppCompatActivity() {
         ) {
         } else {
             requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO), REQUEST_RECORD_AUDIO)
+        }
+    }
+
+    private fun requestSmsSendingPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.SEND_SMS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.SEND_SMS), REQUEST_SEND_SMS)
         }
     }
 }
