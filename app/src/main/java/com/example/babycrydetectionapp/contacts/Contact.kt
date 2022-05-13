7e7dd13702f2a4cf225bf4fe1011f064cd77dab1
chimi-aku <lukaszterpilowski9@gmail.com>
2022-05-13 11:59:19 +0200

    configurtion of roomDB dependecies

--- app/src/main/java/com/example/babycrydetectionapp/contacts/Contact.kt
+++ app/src/main/java/com/example/babycrydetectionapp/contacts/Contact.kt
@@ -1,5 +1,14 @@
 package com.example.babycrydetectionapp.contacts
 
 import android.graphics.drawable.Drawable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-data class Contact(val name: String, val number: String, val image: Drawable?)
+
+data class Contact(
+
+    val name: String,
+    val number: String,
+    val image: Drawable?
+
+    )
